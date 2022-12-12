f891c89c0afa2314d0bd2a943a74dfa700b780b8
Sargeanthost <sargeanthost@gmail.com>
2022-12-11 22:45:26 -0500

    hmmmm. bitset array for column and major and minor diag; no object to save on memory

--- src/Board.java
+++ src/Board.java
@@ -1,27 +1,26 @@
-import java.util.Arrays;
 import java.util.HashSet;
 
 public class Board {
     /**
      * index is 0 to n-1, while elements are 1 to n
      */
-    int[] board;
+    int[] queenPositions;
     /**
-     * The row on which the last queen is found. for a full board where n=8, this would be 7;
+     * The row index on which the last queen is found. for a full board where n=8, this would be 7;
      */
-    private int lastEntry;
-    private final int n; //nxn
+    private int lastEntryIndex;
+    final int n; //nxn
 
-    public Board(int[] board, int lastEntry) {
-        this.board = board;
-        this.lastEntry = lastEntry;
+    public Board(int[] board, int lastEntryIndex) {
+        this.queenPositions = board;
+        this.lastEntryIndex = lastEntryIndex;
         n = board.length;
     }
 
     public Board() {
         n = 8;
-        board = new int[n];
-        lastEntry = -1;
+        queenPositions = new int[n];
+        lastEntryIndex = -1;
     }
 
     /**
@@ -31,31 +30,35 @@ public class Board {
      * @param n     the size of the board
      * @return returns whether the board is in a legal position
      */
-    public static boolean isLegalPosition(int[] board, int n) {
+    public static boolean isLegalPosition(Board board, int n) {
         //col check
         HashSet<Integer> seen = new HashSet<>();
-        for (int position : board) {
-            if (!seen.add(position)) {
+        for(int column = 0; column <= board.lastEntryIndex; column++){
+            if (!seen.add(board.queenPositions[column])) {
                 return false;
             }
         }
         //maj min diag check.
         //offsets should be equal if they're on the same diagonal. trivial to see if on main diag where (0,0),(1,1)
         // interfere: 0-1 = -1, 0-1 = -1.
-        for (int currentRow = 0; currentRow < n; currentRow++) {
-            for (int targetRow = currentRow + 1; targetRow < n; targetRow++) { //if we interfere with a previous
+        for (int currentRow = 0; currentRow <= board.lastEntryIndex; currentRow++) {
+            for (int targetRow = currentRow + 1; targetRow <= board.lastEntryIndex; targetRow++) { //if we interfere with a previous
                 // row, then that previous row interferes with us, so start at +1 previous.
                 if (targetRow == currentRow) {
                     continue;
                 }
                 int rowDif = Math.abs(currentRow - targetRow);
-                int colDif = Math.abs(board[currentRow] - board[targetRow]);
+                int colDif = Math.abs(board.queenPositions[currentRow] - board.queenPositions[targetRow]);
                 if (colDif == rowDif) {
                     return false;
                 }
             }
         }
         return true;
+    }
+
+    public static int solutionNumber(int n) {
+        return 0;
     }
 
     /**
@@ -65,11 +68,11 @@ public class Board {
      * @throws Exception throws an exception if the board is already full and a queen tries to be added to the board.
      */
     private void addQueen(int col) throws Exception {
-        if (lastEntry == board.length - 1) {
+        if (lastEntryIndex == queenPositions.length - 1) {
             throw new Exception("Cannot insert a queen into an already full board.");
         } else if (col < 1 || col > 8) {
             throw new Exception("Cannot place a queen off of the board.");
         }
-        board[lastEntry++] = col;
+        queenPositions[lastEntryIndex++] = col;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,43 @@
 public class Main {
     public static void main(String[] args) {
         testIsLegalAndEnglingBoard();
+
+        testNumberOfSolutionsForN();
     }
 
     private static void testIsLegalAndEnglingBoard() {
-        //{row,column}
-        int[] engling = {1, 6, 8, 3, 7, 4, 2, 5};
-        int[] colBad = {1, 1, 1, 1, 1, 1, 1, 1};
-        int[] majorBad = {1, 2, 3, 4, 5, 6, 7, 8};
-        int[] minorBad = {8, 7, 6, 5, 4, 3, 2, 1};
-        System.out.println("Engling's board is correct: " + Board.isLegalPosition(engling, 8));
-        System.out.println("All in first column is correct: " + Board.isLegalPosition(colBad, 8));
-        System.out.println("All in major diag is correct: " + Board.isLegalPosition(majorBad, 8));
-        System.out.println("All in minor diag is correct: " + Board.isLegalPosition(minorBad, 8));
+        //arr[row] = {column}
+        Board engling = new Board(new int[] {1, 6, 8, 3, 7, 4, 2, 5}, 7);
+        Board colBad = new Board(new int[] {1, 1, 1, 1, 1, 1, 1, 1}, 7);
+        Board majorBad = new Board(new int[] {1, 2, 3, 4, 5, 6, 7, 8}, 7);
+        Board minorBad = new Board(new int[] {8, 7, 6, 5, 4, 3, 2, 1}, 7);
+        Board partialGood = new Board(new int[] {1,6,8,3,7,0,0,0}, 4);
+        Board partialBad = new Board(new int[] {1,6,8,3,5,0,0,0}, 4);
+        System.out.println("Engling's board is correct: " + Board.isLegalPosition(engling, engling.n));
+        System.out.println("All in first column is correct: " + Board.isLegalPosition(colBad, colBad.n));
+        System.out.println("All in major diag is correct: " + Board.isLegalPosition(majorBad, majorBad.n));
+        System.out.println("All in minor diag is correct: " + Board.isLegalPosition(minorBad, minorBad.n));
+        System.out.println("All in partial good is correct: " + Board.isLegalPosition(partialGood, partialGood.n));
+        System.out.println("All in partial bad is correct: " + Board.isLegalPosition(partialBad, partialBad.n));
+
+        //The position returns false when the diagonals or columns collide
+        //An elegant way to be to represent this with just bits, and shift into position
+    }
+
+    private static void testNumberOfSolutionsForN(){
+        assert Board.solutionNumber(4) == 2;
+        assert Board.solutionNumber(5) == 10;
+        assert Board.solutionNumber(6) == 4;
+        assert Board.solutionNumber(7) == 40;
+        assert Board.solutionNumber(8) == 92;
+        assert Board.solutionNumber(9) == 352;
+        assert Board.solutionNumber(10) == 724;
+        assert Board.solutionNumber(11) == 2680;
+        assert Board.solutionNumber(12) == 14200;
+        assert Board.solutionNumber(13) == 73712;
+        assert Board.solutionNumber(14) == 365596;
+        assert Board.solutionNumber(15) == 2279184;
+        assert Board.solutionNumber(16) == 14772512;
+        assert Board.solutionNumber(17) == 95815104;
     }
 }
